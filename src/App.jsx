bfc9d42571ad1c47514000f4393f29524180ef68
caussad@gmail.com <caussad@gmail.com>
2026-04-02 14:49:44 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main className="app">
+      <h1>Vite + React listo</h1>
+      <p>Este proyecto ya tiene la base mínima para empezar a trabajar.</p>
+    </main>
+  )
+}
