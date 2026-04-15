11ad3a67df53559f440a15af30efd12d6e411465
rosalesAlan <varga.alan5725@gmail.com>
2026-04-14 23:58:06 -0300

    Ultimos ajustes del carrusel, ya se puede implementar libremente

--- src/components/CatalogoComp.jsx
+++ src/components/CatalogoComp.jsx
@@ -7,24 +7,55 @@ const Catalogo = ({ items }) => {
   const [modoGrid, setModoGrid] = useState(true);
   
   const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 1024 },
+      items: 4,
+      slidesToSlide: 4
+    },
     desktop: {
-      breakpoint: { max: 3000, min: 1024 },
+      breakpoint: { max: 1024, min: 1024 },
       items: 3,
-      slidesToSlide: 1
+      slidesToSlide: 4
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 2,
-      slidesToSlide: 1
+      slidesToSlide: 4
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
       items: 1,
-      slidesToSlide: 1
+      slidesToSlide: 4
     }
   };
 
-  const renderGrid = () => (
+  
+  const renderCarousel = () => (
+    <Carousel 
+      variant="dark" 
+      responsive={responsive}
+      infinite={true}
+      keyBoardControl={false}
+      containerClass="carousel-container"
+      itemClass="px-2"
+      className="mb-4">
+      {items.map((item) => (
+        <div key={item.id} className="d-flex justify-content-center">
+          <Card style={{ width: '20rem' }}>
+            {item.imagen && <Card.Img variant="top" src={item.imagen} alt={item.nombre} />}
+            <Card.Body>
+              <Card.Title>{item.nombre}</Card.Title>
+              <Card.Text>{item.descripcion}</Card.Text>
+              <Card.Text className="fw-bold">Precio: {item.precio}</Card.Text>
+              <Button variant="primary">Comprar</Button>
+            </Card.Body>
+          </Card>
+        </div>
+      ))}
+    </Carousel>
+  );
+
+    const renderGrid = () => (
     <Row xs={2} sm={2} md={3} lg={4} className="g-4">
       {items.map((item) => (
         <Col key={item.id}>
@@ -40,32 +71,6 @@ const Catalogo = ({ items }) => {
         </Col>
       ))}
     </Row>
-  );
-  
-  const renderCarousel = () => (
-    <Carousel 
-      variant="dark" 
-      responsive={responsive}
-      infinite={true}
-      autoPlay={false}
-      keyBoardControl={true}
-      containerClass="carousel-container"
-      itemClass="px-2"
-      className="mb-4">
-      {items.map((item) => (
-        <div key={item.id} className="d-flex justify-content-center">
-          <Card style={{ width: '22rem' }}>
-            {item.imagen && <Card.Img variant="top" src={item.imagen} alt={item.nombre} />}
-            <Card.Body>
-              <Card.Title>{item.nombre}</Card.Title>
-              <Card.Text>{item.descripcion}</Card.Text>
-              <Card.Text className="fw-bold">Precio: {item.precio}</Card.Text>
-              <Button variant="primary">Comprar</Button>
-            </Card.Body>
-          </Card>
-        </div>
-      ))}
-    </Carousel>
   );
 
   return (
